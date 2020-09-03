d6e1b89764e8bd921b983d08cd190589cfe9c5c0
Lina Montrimaite <l.montrimaite@gmail.com>
2020-09-03 16:00:01 -0700

    Add template file via upload

---
+++ data-wrangling-template.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Wrangling Template
+
+# ## Gather
+
+# In[ ]:
+
+
+
+
+
+# ## Assess
+
+# In[ ]:
+
+
+
+
+
+# ## Clean
+
+# #### Define
+
+# #### Code
+
+# In[ ]:
+
+
+
+
+
+# #### Test
+
+# In[ ]:
+
+
+
+
